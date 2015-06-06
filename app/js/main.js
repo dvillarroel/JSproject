ae579c11da620190cddcf32bc639dee80868a5d6
dvillarroel <maruvisa@hotmail.com>
2015-06-06 08:45:19 -0400

    commit from webstorm

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jalatraining on 5/30/2015.
+ */
+console.log('message from main file');
+var name='new name';
+var sayhello = function(name){
+
+    console.log("dfrom fruntion " + name);
+
+
+};
