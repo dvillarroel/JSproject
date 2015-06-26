1ba461de6ef2e6321e74148e7cb577389864a685
dvillarroel <maruvisa@hotmail.com>
2015-06-26 17:57:48 -0400

    Updating Training branch

--- front-end/app/js/session2.js
+++ front-end/app/js/session2.js
@@ -1,17 +1,32 @@
 
 
-var result = 0;
-var maxNumber = function(){
-    var position = 1;
-    if (!arguments[position + 1])
-        return result;
-       // if(arguments[position] < arguments[position + 1])
+var maxNumber = function(arguments, posicion){
+    if (posicion== arguments.length - 1)
+        return arguments[posicion];
+    return arguments[posicion] + maxNumber(arguments,posicion + 1);
+};
+
+
+var totalNumber = function(arguments,posicion)
+{
+    if (posicion == arguments.length - 1)
+        return arguments[posicion];
+    return arguments[posicion] + totalNumber(arguments,posicion + 1);
+};
+
+var averageNumber = function(arguments)
+{
+    return totalNumber(arguments,0)/arguments.length;
+};
+
+var maxParm = function(arguments,posicion)
+{
 
 };
 
 
 var showResults = function (data) {
-    console.log("the max number is: " + maxNumber(data));
+    console.log("the max number is: " + maxNumber(data,0));
     console.log("the min number is: " + minNumber(data));
     console.log("the average number is: " + averageNumber(data));
     console.log("the total sum is: " + totalNumber(data));
