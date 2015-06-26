7434e9bd61155556796325d12d7d2ec2b49a7aea
dvillarroel <maruvisa@hotmail.com>
2015-06-26 17:33:33 -0400

    Updating Training branch

---
+++ front-end/app/js/session1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Dante Villarroel on 6/26/2015.
+ */
+
+var sayHello = function(){
+  console.log("Hello");
+};
+
+var sayHelloName = function()
+{
+    var name = prompt("Please enter your name");
+    console.log("Hello "+ name);
+
+};
---
+++ front-end/app/js/session2.js
@@ -0,0 +1,13 @@
+
+
+var result = 0;
+var maxNumber = function(){
+    var position = 1;
+    if (!arguments[position + 1])
+        return result;
+       // if(arguments[position] < arguments[position + 1])
+
+
+
+
+};
