da1556d2c9b7f959ae046bbd00a99b995dc0277a
dvillarroel <maruvisa@hotmail.com>
2015-06-26 17:38:48 -0400

    Updating Training branch

--- front-end/app/js/session2.js
+++ front-end/app/js/session2.js
@@ -7,7 +7,12 @@ var maxNumber = function(){
         return result;
        // if(arguments[position] < arguments[position + 1])
 
+};
 
 
-
+var showResults = function (data) {
+    console.log("the max number is: " + maxNumber(data));
+    console.log("the min number is: " + minNumber(data));
+    console.log("the average number is: " + averageNumber(data));
+    console.log("the total sum is: " + totalNumber(data));
 };
